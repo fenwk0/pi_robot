4460e5137151eff16a5c736da1bd38142c37af13
Richard Evans <evansric@hotmail.com>
2016-03-26 22:41:58 +0000

    Added a scusi file.

--- cameraFollow.py
+++ cameraFollow.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import picamera
 import picamera.array
@@ -14,16 +15,17 @@ import threading
 robohat.init()
 
 max_range = 45
-step_size = 2
+step_size = 3
+runtime = 10
+if len(sys.argv) > 1 and sys.argv[1]:
+    runtime = int(sys.argv[1])
 panId = 0
 tiltId = 1
 panVal = 0
 tiltVal = 0
 
-print('resetting servos...')
 robohat.setServo(panId, panVal)
 robohat.setServo(tiltId, tiltVal)
-print('...servos reset')
 
 def panDown():
     global panVal, panId
@@ -49,7 +51,6 @@ def tiltLeft():
 
 class FollowLight(picamera.array.PiYUVAnalysis):
     def analyse(self, a):
-        print('analysing')
         intensities = a[:, :, 0]
         max_index = np.unravel_index(intensities.argmax(), intensities.shape)
         width = intensities.shape[1]
@@ -72,18 +73,17 @@ class FollowLight(picamera.array.PiYUVAnalysis):
 
 
 try:
-    with picamera.PiCamera(framerate=5) as camera:
+    with picamera.PiCamera(framerate=10) as camera:
         camera.hflip = True
         camera.vflip = True
         print('starting camera')
         with FollowLight(camera) as output:
             camera.resolution = (320, 240)
             camera.start_recording(output, format='yuv')
-            camera.wait_recording(3)
+            camera.wait_recording(runtime)
             camera.stop_recording()
 
-except KeyboardInterrupt:
+finally:
     robohat.setServo(tiltId, 0)
     robohat.setServo(panId, 0)
-finally:
     robohat.cleanup()
